07f4c260dcd4dcb55022eea3504a39919b7891e4
karthikbenki <karthikmc007@gmail.com>
2022-02-22 12:49:17 +0530

    Added: printing int double and char array in  generic way

--- src/com/blz/printarray/PrintArray.java
+++ src/com/blz/printarray/PrintArray.java
@@ -2,31 +2,17 @@ package com.blz.printarray;
 
 public class PrintArray {
 
-    // integer array printing
-    static public void printArray(int[] arr) {
-	for (int element : arr)
-	    System.out.print(element + " ");
-	System.out.println();
-    }
-
-    // double array printing
-    static public void printArray(double[] arr) {
-	for (Double element : arr)
-	    System.out.print(element + " ");
-	System.out.println();
-    }
-
-    // character array printing
-    static public void printArray(char[] arr) {
-	for (char element : arr)
+    // generic array printing
+    static public <T> void printArray(T[] arr) {
+	for (T element : arr)
 	    System.out.print(element + " ");
 	System.out.println();
     }
 
     public static void main(String[] args) {
-	int[] arr1 = { 10, 20, 30 };
-	double[] arr2 = { 1.23, 2.24, 3.345 };
-	char[] arr3 = { 'a', 'A', 'V' };
+	Integer[] arr1 = { 10, 20, 30 };
+	Double[] arr2 = { 1.23, 2.24, 3.345 };
+	Character[] arr3 = { 'a', 'A', 'V' };
 	printArray(arr1);
 	printArray(arr2);
 	printArray(arr3);
